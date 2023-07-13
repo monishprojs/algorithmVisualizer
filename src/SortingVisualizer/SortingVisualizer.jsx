073258d8d6b97671170df04376f7748a336fbe1f
monishprojs <monishkumar833@gmail.com>
2023-07-13 18:42:57 -0400

    merge sort

--- src/SortingVisualizer/SortingVisualizer.jsx
+++ src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,11 +1,16 @@
 import React from 'react';
 import './SortingVisualizer.css';
 import { useEffect, useState } from 'react';
+import getMergeSortAnimations from '../sortingAlgorithms/mergeSort.js';
 
 function SortingVisualizer(){
 
 
     const[nums,setNums] = useState([])
+
+    const PRIMARY_COLOR = 'turquoise';
+    const SECONDARY_COLOR = 'red';
+    const ANIMATION_SPEED_MS = 1;
     
     function resetArray(){
         let tmp = [];
@@ -16,7 +21,27 @@ function SortingVisualizer(){
     }
 
     function mergeSort(){
-        
+        const animations = getMergeSortAnimations(nums);
+        for (let i = 0; i < animations.length; i++) {
+            const arrayBars = document.getElementsByClassName('bar');
+            const isColorChange = i % 3 !== 2;
+            if (isColorChange) {
+                const [barOneIdx, barTwoIdx] = animations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                const barTwoStyle = arrayBars[barTwoIdx].style;
+                const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+                setTimeout(() => {
+                    barOneStyle.backgroundColor = color;
+                    barTwoStyle.backgroundColor = color;
+                }, i * ANIMATION_SPEED_MS);
+            } else {
+                setTimeout(() => {
+                    const [barOneIdx, newHeight] = animations[i];
+                    const barOneStyle = arrayBars[barOneIdx].style;
+                    barOneStyle.height = `${newHeight}px`;
+                }, i * ANIMATION_SPEED_MS);
+            }
+        }
     }
 
     function quickSort() {
