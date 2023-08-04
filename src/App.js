8273a44bca812ce6622b6727518de0d743981629
monishprojs <monishkumar833@gmail.com>
2023-08-04 15:29:32 -0400

    changed default route

--- src/App.js
+++ src/App.js
@@ -18,15 +18,15 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/searching" element={<SearchingVisualizer />} />
+          <Route path="/pathfinding" element={<PathfindingVisualizer />} />
 
-          <Route path="/sorting" element={<SortingVisualizer/>} />
+          <Route path="/sorting" element={<SortingVisualizer />} />
 
           <Route path="/sudoku" element={<SudokuVisualizer />} />
 
-          <Route path="/pathfinding" element={<PathfindingVisualizer />} />
+          <Route path="/searching" element={<SearchingVisualizer />} />
 
-          <Route path="/" element={<Navigate replace to="/sorting" />} />
+          <Route path="/" element={<Navigate replace to="/pathfinding" />} />
         </Routes>
       </Router>
     </div>
